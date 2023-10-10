b61876f1e261590477a1a79150732b1fe4b7c9a1
Ayakhuwailed <aya.khuwailed2000@gmail.com>
2023-10-10 18:56:57 +0300

    add changes

--- cypress/PageObject/addEmployee/employeeAssertions.ts
+++ cypress/PageObject/addEmployee/employeeAssertions.ts
@@ -5,18 +5,4 @@ export default class employeeAssertions {
         cy.location('pathname', {timeout: 60000})
             .should('include', '/pim/viewPersonalDetails/empNumber/');
     }
-
-    // employeeDetails(id: number) {
-    //     return cy.request({
-    //         method: 'GET', url: `/api/v2/pim/employees/${id}/personal-details`,
-    //
-    //     }).then(res => {
-    //         expect(res.body.data.employeeId).to.eq('415')
-    //         expect(res.body.data.firstName).to.eq('test')
-    //         expect(res.body.data.middleName).to.eq('test')
-    //         expect(res.body.data.lastName).to.eq('test')
-    //         cy.log(res.body.data.employeeId)
-    //     })
-    // }
-
 }
